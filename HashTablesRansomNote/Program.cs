99aa5f7e5eff555a83e716ceede21ea78a5df109
Srivardhan Hebbar <srhebbar@microsoft.com>
2016-10-11 13:11:34 +0530

    Challenge Passed

---
+++ HashTablesRansomNote/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTablesRansomNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens = Console.ReadLine().Split();
+            int m = int.Parse(tokens[0]);
+            int n = int.Parse(tokens[1]);
+            string[] magazine = Console.ReadLine().Split();
+            string[] ransomNote = Console.ReadLine().Split();
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            foreach(string word in magazine)
+            {
+                if (words.ContainsKey(word))
+                {
+                    words[word]++;
+                }
+                else
+                {
+                    words.Add(word, 1);
+                }
+            }
+
+            foreach(string word in ransomNote)
+            {
+                if (words.ContainsKey(word))
+                {
+                    words[word]--;
+                    if (words[word] == -1)
+                    {
+                        Console.WriteLine("No");
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Yes");
+        }
+    }
+}
